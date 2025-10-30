a6df373d9873e4415138a8b8e9e4fd78bdbaedf8
Mottel Zirkind <shachna@gmail.com>
2025-10-29 23:12:49 -0400

    fix: removed old import

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ dotenv.config();
 
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const snapdeck = require('snapdeck');
-const parseDeck = require('./parseDeck');
 
 const TOKEN = process.env.NEO_BOT_TOKEN; // Replace with your bot token
 const client = new Client({
@@ -24,18 +23,17 @@ client.on('messageCreate', async (message) => {
 
 	try {
 		const deckcode = snapdeck.extractDeckcode(message.content);
-		console.log(`extracted deckcode: ${JSON.stringify(deckcode)}`);
 		if (!deckcode) return;
+
 		const deck = await snapdeck.parseDeckcode(deckcode);
-		console.log(`parsed deck: ${JSON.stringify(deck)}`);
 		if (!deck) return;
+
 		const displayDeck = await snapdeck.generateDisplayString(deck.cards);
-		console.log(`generated display deck: ${displayDeck}`);
 		if (!displayDeck) return;
 
 		await message.delete();
-
 		await message.channel.send(`Here's the deck <@${message.author.id}> shared:\n\`\`\`${displayDeck}\`\`\``);
+
 	} catch (err) {
 		console.log(`error processing: ${message.content}\n${err}`);
 	}
